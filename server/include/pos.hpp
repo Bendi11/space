c57c1dfbf5c4aac24ba3391f133932a9f5791006
Bendi11 <bkliebmann@gmail.com>
2023-05-17 20:26:31 -0700

    Added millimeter and gridsquare types to prevent accidental assignments between units

--- server/include/pos.hpp
+++ server/include/pos.hpp
@@ -6,32 +6,44 @@
 #include <limits>
 #include <compare>
 
-namespace _impl {
+#define WRAP(ty) \
+    inline constexpr ty operator+(ty const& other) const noexcept { return ty(_v + other._v); } \
+    inline constexpr ty operator-(ty const& other) const noexcept { return ty(_v - other._v); } \
+    inline constexpr ty operator*(ty const& other) const noexcept { return ty(_v * other._v); } \
+    inline constexpr ty operator/(ty const& other) const noexcept { return ty(_v / other._v); } \
+    inline constexpr ty operator%(ty const& other) const noexcept { return ty(_v / other._v); } \
+    inline constexpr auto operator<=>(ty const& other) const noexcept { return _v <=> other._v; };          \
+    inline constexpr ty& operator+=(ty const& other) noexcept { return *this = *this + other; }     \
+    inline constexpr ty& operator-=(ty const& other) noexcept { return *this = *this - other; }     \
+    inline constexpr ty& operator*=(ty const& other) noexcept { return *this = *this * other; }     \
+    inline constexpr ty& operator/=(ty const& other) noexcept { return *this = *this / other; }     \
+    inline constexpr ty& operator%=(ty const& other) noexcept { return *this = *this % other; }
 
-template<std::integral T>
-struct wrapper {
-    using self = wrapper<T>;
 
-    explicit inline constexpr wrapper(T v) noexcept : _v{v} {}
-    explicit inline constexpr operator T() noexcept { return _v; }
-    
-    inline constexpr self operator+(self const& other) const noexcept { return self(_v + other._v); };
-    inline constexpr self operator-(self const& other) const noexcept { return self(_v - other._v); }
-    inline constexpr self operator*(self const& other) const noexcept { return self(_v * other._v); }
-    inline constexpr self operator/(self const& other) const noexcept { return self(_v / other._v); }
-    inline constexpr self operator%(self const& other) const noexcept { return self(_v / other._v); }
-    inline constexpr auto operator<=>(self const& other) const noexcept = default;
+struct millimeter {
+public:
+    WRAP(millimeter)
 
-protected:
-    T _v;
+    constexpr inline millimeter(std::uint64_t v) noexcept : _v(v) {}
+
+    constexpr inline bool chkadd(millimeter& result, millimeter other) const noexcept {
+        result = *this + other;
+        return result < other;
+    }
+
+private:
+    std::uint64_t _v;
 };
 
-
-}
-
-struct millimeter : _impl::wrapper<std::uint64_t> {
-    constexpr inline millimeter(std::uint64_t v) noexcept : _impl::wrapper<std::uint64_t>(v) {}
+struct gridsquare {
+public:
+    WRAP(gridsquare)
+    constexpr inline gridsquare(std::uint64_t v) noexcept : _v(v) {}
+private:
+    std::uint64_t _v;
 };
+
+#undef WRAP
 
 
 /**
@@ -41,18 +53,22 @@ struct universe_pos {
     using distance = std::uint64_t;
     using limits = std::numeric_limits<distance>;
 
-    inline constexpr universe_pos(distance grid, distance mm) : _grid(grid), _mm(mm) {}
+    inline constexpr universe_pos(gridsquare grid, millimeter mm) : _grid(grid), _mm(mm) {}
     
     [[nodiscard]]
     static inline constexpr universe_pos middle() noexcept { return universe_pos(limits::max() / 2, limits::max() / 2); }
 
     inline constexpr universe_pos operator+(universe_pos const& other) const noexcept {
-
+        universe_pos tmp{_grid + other._grid, 0};
+        if(_mm.chkadd(tmp._mm, other._mm)) {
+            tmp._grid += 1;
+        }
+        return tmp;
     }
 
 private:
     /// 1 ~= 18.4467 petamters
-    distance _grid;
+    gridsquare _grid;
     /// 1 = 1 millimeter
     millimeter _mm;
 };
